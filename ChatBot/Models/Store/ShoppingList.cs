68b741f3fd3f09e0bf056ccbe54c589c039fb7d7
Yarden <yarden2100@gmail.com>
2018-09-01 23:38:28 +0300

    First commit

---
+++ ChatBot/Models/Store/ShoppingList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Models
+{
+    public class ShoppingList
+    {
+        # region members
+
+        public List<Item> Items { get; set; };
+
+        #endregion
+    }
+}
